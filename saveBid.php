64d0626a2441ff066d0f889968999d968300f351
matt-evans50 <MatthewEvans2014@u.northwestern.edu>
2013-10-17 20:57:34 -0500

    Update saveBid.php

--- saveBid.php
+++ saveBid.php
@@ -7,7 +7,7 @@ echo "<script language='javascript'>";
 echo "alert('message successfully sent')";
 echo "</script>";
 
-/*
+
 if(!empty($fullname) && !empty($bid))
 {
 	$MYSQL_server_name = "mysql121.freehostia.com:3306";
@@ -31,6 +31,6 @@ if(!empty($fullname) && !empty($bid))
 	if(!@mysql_query($query))
 	{ echo "<script type='text/javascript/>alert('Unsuccessfully entered')</script>"; }
 }
-*/
+
 
 ?>
