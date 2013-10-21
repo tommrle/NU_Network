bafe880cc07f15e5a15d95ae01988179a70d94e1
matt-evans50 <MatthewEvans2014@u.northwestern.edu>
2013-10-20 19:37:16 -0500

    Update saveBid.php

--- saveBid.php
+++ saveBid.php
@@ -1,37 +1,50 @@
 <?php
-/*
-$fullname = $_POST['fullname'];
-$bid = $_POST['bid'];
-$classname = $_POST['classname'];
-*/
+
+$fullname = addslashes($_POST['fullname']);
+$bid = addslashes($_POST['bid']);
+$classname = addslashes($_POST['class']);
+$contact = addslashes($_POST['contact']);
+
+
 
 echo '<script language="javascript">';
-echo 'alert("message successfully sent")';
+echo 'alert("Improper form input! Price must be an integer!")';
 echo '</script>';
-/*
+header( 'Location: http://liuzy.net/jquerysite.php#sellpage');
+
+if (empty($fullname))
+{
+	echo "empty";
+}
+
+
+
 if(!empty($fullname) && !empty($bid))
 {
-	$MYSQL_server_name = "mysql121.freehostia.com:3306";
-	$MYSQL_server_username = "zhaliu0";
+	$MYSQL_server_name = "mysql21.freehostia.com";
+	$MYSQL_server_username = "zhaliu0_market";
 	$MYSQL_server_password = "0077049";
 	$DB_name = "zhaliu0_market";
 
-	$server = @mysql_connect($MYSQL_server_name, $MYSQL_server_username, $MYSQL_server_password);
+	$server = @mysqli_connect($MYSQL_server_name, $MYSQL_server_username, $MYSQL_server_password, $DB_name );
 
 	if (!$server) {
 	echo "Could not get server";
 	exit();
 	}
 
-	$db = @mysql_select_db($DB_name);
-	if (!$db) {
-	echo "Could not get database";
-	exit();
-	}	
-	$query="INSERT INTO books (classname,seller,sellprice) VALUES('$classname','$fullname',$bid);
-	if(!@mysql_query($query))
-	{ echo "<script type='text/javascript/>alert('Unsuccessfully entered')</script>"; }
+	mysqli_select_db($server,"zhaliu0_market");
+		
+	$query="INSERT INTO books (classname,seller,sellprice,contact) VALUES('$classname','$fullname',$bid,'$contact')";
+	if(!mysqli_query($server,$query)){ 
+		echo "<script type='text/javascript/>alert('Unsuccessfully entered')</script>";
+		header( 'Location: http://liuzy.net/jquerysite.php#failed' );
+	}
+	else {
+		echo "query success:  $query";
+		header( 'Location: http://liuzy.net/jquerysite.php#confirmed' );
+	}
 }
-*/
+   
 
 ?>
